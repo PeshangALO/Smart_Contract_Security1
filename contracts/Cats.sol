fac4935c78311a12dcb19e80d4a1340dfc0a81b1
Peshang <peseng88@gmail.comn>
2021-03-05 12:52:55 +0100

    Created Cats contract with git and set

---
+++ contracts/Cats.sol
@@ -0,0 +1,22 @@
+pragma solidity >=0.4.22 <0.9.0;
+
+import"./Storage.sol";
+
+contract Cats is Storage{
+
+    modifier onlyOwner{
+        require(msg.sender == owner);
+        _;
+    }
+    
+constructor() public{
+    owner = msg.sender;
+}
+
+    function getNumberOFCats()public view returns(uint256){
+        return _uintStorage["Cats"];
+    }
+    function setNumberOfCats(uint256 _toSet)public {
+        _uintStorage["Cats"] = _toSet;
+    }
+}
